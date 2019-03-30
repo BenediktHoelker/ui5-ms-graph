6c792fa1efbeefc66a221c37d8a176dc1db228f8
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2019-03-30 21:26:48 +0100

    Authentication flow

--- webapp/controller/Authentication.controller.js
+++ webapp/controller/Authentication.controller.js
@@ -1,111 +1,68 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], function(Controller) {
-  "use strict";
+sap.ui.define(
+  [
+    "com/iot/ui5-ms-graph/msal/config",
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+  ],
+  function(applicationConfig, Controller, JSONModel) {
+    "use strict";
 
-  var applicationConfig = {
-    clientID: "94bb3d16-4be3-4020-b929-ec0ca5a4103a",
-    authority: "https://login.microsoftonline.com/organizations",
-    graphScopes: ["user.read"],
-    graphEndpoint: "https://graph.microsoft.com/v1.0/me"
-  };
-
-  var myMSALObj = new Msal.UserAgentApplication(
-    applicationConfig.clientID,
-    applicationConfig.authority,
-    acquireTokenRedirectCallBack,
-    { storeAuthStateInCookie: true, cacheLocation: "localStorage" }
-  );
-
-  function signIn() {
-    myMSALObj.loginPopup(applicationConfig.graphScopes).then(
-      function(idToken) {
-        //Login Success
-        acquireTokenPopupAndCallMSGraph();
-      },
-      function(error) {
-        console.log(error);
-      }
+    var myMSALObj = new Msal.UserAgentApplication(
+      applicationConfig.clientID,
+      applicationConfig.authority,
+      acquireTokenRedirectCallBack,
+      { storeAuthStateInCookie: true, cacheLocation: "localStorage" }
     );
-  }
 
-  function signOut() {
-    myMSALObj.logout();
-  }
+    function acquireTokenRedirectCallBack(errorDesc, token, error, tokenType) {
+      if (tokenType === "access_token") {
+        callMSGraph(applicationConfig.graphEndpoint, token, graphAPICallback);
+      } else {
+        console.log("token type is:" + tokenType);
+      }
+    }
 
-  function acquireTokenPopupAndCallMSGraph() {
-    //Call acquireTokenSilent (iframe) to obtain a token for Microsoft Graph
-    myMSALObj.acquireTokenSilent(applicationConfig.graphScopes).then(
-      function(accessToken) {
-        // new sap.ui.model.odata.v2.ODataModel("https://graph.microsoft.com/v1.0", {
-        //  headers: { Authorization: "Bearer " + accessToken }
-        // });
-        sap.ui.getCore().BenesModel = new sap.ui.model.odata.v4.ODataModel({
-          earlyRequests: true,
-          serviceUrl: "https://graph.microsoft.com/v1.0/me/",
-          synchronizationMode: "None"
-        });
-        callMSGraph(
-          applicationConfig.graphEndpoint,
-          accessToken,
-          graphAPICallback
+    function signOut() {
+      myMSALObj.logout();
+    }
+
+    return Controller.extend("com.iot.ui5-ms-graph.controller.InitialView", {
+      onInit: function() {
+        var sessionModel = new JSONModel();
+        this.getOwnerComponent().setModel(sessionModel, "session");
+      },
+
+      signIn: function() {
+        var sessionModel = this.getOwnerComponent().getModel("session");
+
+        myMSALObj.loginPopup(applicationConfig.graphScopes).then(
+          function(idToken) {
+            myMSALObj
+              .acquireTokenSilent(applicationConfig.graphScopes)
+              .then(function(accessToken) {
+                sessionModel.setProperty("/token", accessToken);
+                return accessToken;
+              })
+              .then(function(accessToken) {
+                return fetch(applicationConfig.graphEndpoint, {
+                  method: "GET", // or 'PUT'
+                  headers: { Authorization: "Bearer " + accessToken }
+                });
+              })
+              .then(res => res.json())
+              .then(response =>
+                sessionModel.setProperty("/userData", response)
+              );
+          },
+          function(error) {
+            console.log(error);
+          }
         );
       },
-      function(error) {
-        console.log(error);
-        // Call acquireTokenPopup (popup window) in case of acquireTokenSilent failure due to consent or interaction required ONLY
-        if (
-          error.indexOf("consent_required") !== -1 ||
-          error.indexOf("interaction_required") !== -1 ||
-          error.indexOf("login_required") !== -1
-        ) {
-          myMSALObj.acquireTokenPopup(applicationConfig.graphScopes).then(
-            function(accessToken) {
-              callMSGraph(
-                applicationConfig.graphEndpoint,
-                accessToken,
-                graphAPICallback
-              );
-            },
-            function(error) {
-              console.log(error);
-            }
-          );
-        }
+
+      signOut: function() {
+        signOut();
       }
-    );
+    });
   }
-
-  function callMSGraph(theUrl, accessToken, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200)
-        callback(JSON.parse(this.responseText));
-    };
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.setRequestHeader("Authorization", "Bearer " + accessToken);
-    xmlHttp.send();
-  }
-
-  function graphAPICallback(data) {
-    //Display user data on DOM
-    console.log(JSON.stringify(data));
-  }
-
-  function acquireTokenRedirectCallBack(errorDesc, token, error, tokenType) {
-    if (tokenType === "access_token") {
-      callMSGraph(applicationConfig.graphEndpoint, token, graphAPICallback);
-    } else {
-      console.log("token type is:" + tokenType);
-    }
-  }
-
-  return Controller.extend("com.iot.ui5-ms-graph.controller.InitialView", {
-    onInit: function() {},
-    signIn: function() {
-      signIn();
-    },
-
-    signOut: function() {
-      signOut();
-    }
-  });
-});
+);
---
+++ webapp/msal/config.js
@@ -0,0 +1,10 @@
+sap.ui.define([], function() {
+  "use strict";
+
+  return {
+    clientID: "94bb3d16-4be3-4020-b929-ec0ca5a4103a",
+    authority: "https://login.microsoftonline.com/organizations",
+    graphScopes: ["user.read"],
+    graphEndpoint: "https://graph.microsoft.com/v1.0/me"
+  };
+});
