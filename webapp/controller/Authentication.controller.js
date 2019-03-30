02ae442bbb62079b431e5981d8ca4307092c3b37
Benedikt Hölker <benedikt.hoelker@iot-online.de>
2019-03-30 21:41:27 +0100

    Logout

--- webapp/controller/Authentication.controller.js
+++ webapp/controller/Authentication.controller.js
@@ -1,49 +1,36 @@
 sap.ui.define(
-  [
-    "com/iot/ui5-ms-graph/msal/config",
-    "sap/ui/core/mvc/Controller",
-  ],
+  ["com/iot/ui5-ms-graph/msal/config", "sap/ui/core/mvc/Controller"],
   function(applicationConfig, Controller) {
     "use strict";
 
     var myMSALObj = new Msal.UserAgentApplication(
       applicationConfig.clientID,
       applicationConfig.authority,
-      acquireTokenRedirectCallBack,
+      null,
       { storeAuthStateInCookie: true, cacheLocation: "localStorage" }
     );
 
-    function acquireTokenRedirectCallBack(errorDesc, token, error, tokenType) {
-      if (tokenType === "access_token") {
-        callMSGraph(applicationConfig.graphEndpoint, token, graphAPICallback);
-      } else {
-        console.log("token type is:" + tokenType);
-      }
-    }
-
-    function signOut() {
-      myMSALObj.logout();
-    }
-
     return Controller.extend("com.iot.ui5-ms-graph.controller.InitialView", {
       onInit: function() {
-				this._signIn();
+        this._signIn();
       },
 
-      signIn: function() {
-        myMSALObj.loginPopup(applicationConfig.graphScopes).then(
-          function() {
-						this._signIn();
-					},
-          function(error) {
-            console.log(error);
-          }
-        );
+      onSwitchSession: function() {
+        var sessionModel = this.getOwnerComponent().getModel("session");
+        var isLoggedIn = sessionModel.getProperty("/userData/displayName");
+        if (isLoggedIn) {
+          myMSALObj.logout();
+        } else {
+          myMSALObj
+            .loginPopup(applicationConfig.graphScopes)
+            .then(() => this._signIn())
+            .catch(error => console.log(error));
+        }
       },
 
       _signIn: function() {
-				var sessionModel = this.getOwnerComponent().getModel("session");
-				
+        var sessionModel = this.getOwnerComponent().getModel("session");
+
         myMSALObj
           .acquireTokenSilent(applicationConfig.graphScopes)
           .then(function(accessToken) {
@@ -58,10 +45,6 @@ sap.ui.define(
           })
           .then(res => res.json())
           .then(response => sessionModel.setProperty("/userData", response));
-      },
-
-      signOut: function() {
-        signOut();
       }
     });
   }
